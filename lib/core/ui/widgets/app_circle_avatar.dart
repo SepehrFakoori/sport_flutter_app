bd9f9ea2b1054d39824c0bf85bbcdd90ea92caf0
Sepehr <sepehrfakoori@gmail.com>
2026-05-23 11:52:59 +0330

    ref(avatar): add backgroundImage to CircleAvatar widget

--- lib/core/ui/widgets/app_circle_avatar.dart
+++ lib/core/ui/widgets/app_circle_avatar.dart
@@ -1,24 +1,35 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:sport_flutter_app/core/extensions/build_context_extensions.dart';
 
 class AppCircleAvatar extends StatelessWidget {
   final String username;
   final String? imageUrl;
+  final double? radius;
 
-  const AppCircleAvatar({super.key, required this.username, this.imageUrl});
+  const AppCircleAvatar({
+    super.key,
+    required this.username,
+    this.imageUrl,
+    this.radius = 30,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final bool hasImage = imageUrl != null && imageUrl!.isNotEmpty;
+
     return CircleAvatar(
-      radius: 30,
+      radius: radius,
       backgroundColor: context.colors.primary,
-      // TODO: user backgroundImage: CachedNetworkImageProvider() for network images
-      child: Text(
-        username[0].toUpperCase(),
-        style: context.textTheme.headlineLarge!.copyWith(
-          color: context.colors.onPrimary,
-        ),
-      ),
+      backgroundImage: hasImage ? CachedNetworkImageProvider(imageUrl!) : null,
+      child: !hasImage
+          ? Text(
+              username[0].toUpperCase(),
+              style: context.textTheme.headlineLarge!.copyWith(
+                color: context.colors.onPrimary,
+              ),
+            )
+          : null,
     );
   }
 }
