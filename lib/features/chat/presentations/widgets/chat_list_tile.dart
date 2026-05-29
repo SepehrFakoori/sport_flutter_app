194cdea0945cafa683dd46ed4d4409c31b9e06a3
Sepehr <sepehrfakoori@gmail.com>
2026-05-29 16:27:06 +0330

    feat(chat): complete chat list

--- lib/features/chat/presentations/widgets/chat_list_tile.dart
+++ lib/features/chat/presentations/widgets/chat_list_tile.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:sport_flutter_app/core/extensions/build_context_extensions.dart';
+import 'package:sport_flutter_app/core/ui/widgets/app_badge.dart';
 import 'package:sport_flutter_app/core/ui/widgets/app_circle_avatar.dart';
+import 'package:sport_flutter_app/features/chat/presentations/widgets/coach_label.dart';
+
+enum ChatType { coach, user, support }
 
 class ChatListTile extends StatelessWidget {
-  final String username;
-  final String subtitle;
+  final String firstname;
+  final String lastname;
+  final String lastMessage;
   final GestureTapCallback? onTap;
   final GestureLongPressCallback? onLongPress;
+  final ChatType? type;
 
   const ChatListTile({
     super.key,
-    required this.username,
-    required this.subtitle,
+    required this.firstname,
+    required this.lastname,
+    required this.lastMessage,
     this.onTap,
     this.onLongPress,
+    this.type,
   });
 
   @override
@@ -24,16 +32,43 @@ class ChatListTile extends StatelessWidget {
       contentPadding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
       horizontalTitleGap: 12,
       titleAlignment: ListTileTitleAlignment.center,
-      leading: AppCircleAvatar(username: username, radius: 28),
-      title: Text(username),
+      isThreeLine: false,
+      leading: AppCircleAvatar(username: '$firstname $lastname', radius: 28),
+      title: Row(
+        mainAxisAlignment: .start,
+        crossAxisAlignment: .center,
+        spacing: 4,
+        children: [
+          Flexible(
+            child: Text(
+              '$firstname $lastname',
+              maxLines: 1,
+              overflow: .ellipsis,
+            ),
+          ),
+          ?_getChatLabel(type),
+        ],
+      ),
       titleTextStyle: context.textTheme.headlineSmall?.copyWith(
         fontWeight: .w600,
+        height: 1.5,
       ),
-      subtitle: Text(subtitle),
+      subtitle: Text(lastMessage, maxLines: 1, overflow: .ellipsis),
       subtitleTextStyle: context.textTheme.bodyMedium?.copyWith(
         fontWeight: .w500,
         color: context.colors.textSecondary,
       ),
     );
+  }
+
+  Widget? _getChatLabel(ChatType? type) {
+    switch (type) {
+      case .coach:
+        return CoachLabel();
+      case .support:
+        return SupportLabel();
+      default:
+        return null;
+    }
   }
 }
--- lib/features/home/presentations/home_page.dart
+++ lib/features/home/presentations/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sport_flutter_app/core/constants/assets_icons.dart';
 import 'package:sport_flutter_app/core/extensions/build_context_extensions.dart';
+import 'package:sport_flutter_app/core/ui/widgets/buttons/app_icon_button.dart';
+import 'package:sport_flutter_app/core/ui/widgets/icon_widget.dart';
 import 'package:sport_flutter_app/core/ui/widgets/image_loader.dart';
 import 'package:sport_flutter_app/features/home/presentations/widgets/category_list.dart';
 import 'package:sport_flutter_app/features/home/presentations/widgets/coach_tile.dart';
