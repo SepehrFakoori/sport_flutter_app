26cfd50e6f2733352bc3acaf4364a7417193779a
sahand <sahandfakoori99@gmail.com>
2026-06-05 12:00:12 +0330

    ref(card): refactor class list view

--- lib/features/home/presentations/home_page.dart
+++ lib/features/home/presentations/home_page.dart
@@ -6,7 +6,7 @@ import 'package:sport_flutter_app/core/router/app_routes.dart';
 import 'package:sport_flutter_app/core/ui/widgets/buttons/app_icon_button.dart';
 import 'package:sport_flutter_app/core/ui/widgets/icon_widget.dart';
 import 'package:sport_flutter_app/features/home/presentations/widgets/category_list.dart';
-import 'package:sport_flutter_app/features/home/presentations/widgets/class_card.dart';
+import 'package:sport_flutter_app/features/home/presentations/widgets/class_list_view.dart';
 import 'package:sport_flutter_app/features/home/presentations/widgets/filter_tile.dart';
 
 class HomePage extends StatelessWidget {
@@ -84,33 +84,6 @@ class HomePage extends StatelessWidget {
               ClassListView(),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class ClassListView extends StatelessWidget {
-  const ClassListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 280,
-      child: ListView.builder(
-        itemCount: 8,
-        scrollDirection: Axis.horizontal,
-        padding: EdgeInsets.symmetric(horizontal: 8),
-        itemBuilder: (context, index) => ClassCard(
-          imageUrl:
-              'https://teempo.ir/wp-content/uploads/2025/04/167027126-H-1024x700-copy.webp',
-          title: 'یوگا صبحگاهی',
-          firstname: 'فاطمه',
-          lastname: 'اکبری',
-          location: 'تهران، سعادت آباد',
-          capacity: 20,
-          remaining: 16,
-          cost: '1,800,000',
         ),
       ),
     );
