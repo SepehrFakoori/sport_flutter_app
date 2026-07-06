e2991e62439c6c0797bd7ee61a4d96e946f114f5
Sahand Fakoori <sahandfakoori99@gmail.com>
2026-07-06 17:16:33 +0330

    feat(complete-profile): create complete profile screen

---
+++ lib/features/profile/presentation/bloc/complete_profile/complete_profile_event.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+sealed class CompleteProfileEvent extends Equatable {
+  const CompleteProfileEvent();
+
+  @override
+  List<Object?> get props => [];
+}
