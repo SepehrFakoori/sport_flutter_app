e7cd9f6962117f7207947e3e87fc4ffd1e9cf173
Sepehr <sepehrfakoori@gmail.com>
2026-07-11 16:18:04 +0330

    ref(profile): refactor profile complete button alignment

--- lib/features/profile/presentation/complete_profile_screen.dart
+++ lib/features/profile/presentation/complete_profile_screen.dart
@@ -45,131 +45,144 @@ class _CompleteProfileScreenState extends State<CompleteProfileScreen> {
       },
       builder: (context, state) {
         return Scaffold(
+          resizeToAvoidBottomInset: false,
           body: SafeArea(
-            child: Padding(
-              padding: const .symmetric(horizontal: 16.0, vertical: 24),
-              child: Column(
-                crossAxisAlignment: .start,
-                spacing: 24,
-                children: [
-                  Text(
-                    context.l10n.profile_complete_title,
-                    style: context.textTheme.headlineMedium,
-                  ),
-                  AppTextFormField(
-                    labelText: context.l10n.profile_firstname_title,
-                    keyboardType: .name,
-                    autofocus: true,
-                    textInputAction: .next,
-                    errorText: _translateFirstNameException(
-                      context,
-                      state.exception,
-                    ),
-                    onChange: (value) => bloc.add(FirstNameChanged(value)),
-                  ),
-                  AppTextFormField(
-                    labelText: context.l10n.profile_lastname_title,
-                    keyboardType: .name,
-                    autofocus: true,
-                    textInputAction: .next,
-                    errorText: _translateLastNameException(
-                      context,
-                      state.exception,
-                    ),
-                    onChange: (value) => bloc.add(LastNameChanged(value)),
-                  ),
-                  GenderSelection(
-                    selected: {state.gender},
-                    onSelectionChanged: (gender) =>
-                        bloc.add(GenderChanged(gender.single)),
-                  ),
-                  AppTextFormField(
-                    labelText: context.l10n.profile_optional_email_title,
-                    keyboardType: .emailAddress,
-                    autofocus: true,
-                    textInputAction: .done,
-                    errorText: _translateEmailException(
-                      context,
-                      state.exception,
-                    ),
-                    onChange: (value) => bloc.add(EmailChanged(value)),
-                  ),
-                  AppTextFormField(
-                    controller: _birthDateController,
-                    onTap: () {
-                      AppModalBottomSheet.show(
-                        context,
-                        child: Column(
-                          mainAxisSize: .min,
-                          children: [
-                            PersianCalendarDatePicker(
-                              initialDate: null,
-                              firstDate: Jalali(1300),
-                              lastDate: Jalali.now(),
-                              initialCalendarMode: .year,
-                              onDateChanged: (Jalali picked) {
-                                _birthDateController.text = picked
-                                    .formatShortDate();
-                                bloc.add(BirthDateChanged(picked.toDateTime()));
-                              },
-                            ),
-                            Padding(
-                              padding: const .symmetric(
-                                vertical: 8.0,
-                                horizontal: 16.0,
-                              ),
-                              child: Row(
-                                mainAxisAlignment: .end,
-                                spacing: 8,
-                                children: [
-                                  Expanded(
-                                    child: AppOutlinedButton.secondary(
-                                      onPressed: () => context.pop(),
-                                      title: context.l10n.global_button_cancel,
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: AppFilledButton.secondary(
-                                      onPressed: () => context.pop(),
-                                      title: context.l10n.global_button_save,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                    labelText: context.l10n.profile_birthdate_title,
-                    canRequestFocus: false,
-                    readOnly: true,
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: IconWidget(
-                        icon: AssetIcons.calendar,
-                        height: 10,
-                        width: 10,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const .symmetric(horizontal: 16.0, vertical: 24),
+                  child: Column(
+                    crossAxisAlignment: .start,
+                    mainAxisSize: .max,
+                    spacing: 24,
+                    children: [
+                      Text(
+                        context.l10n.profile_complete_title,
+                        style: context.textTheme.headlineMedium,
                       ),
-                    ),
-                    // errorText: _translateBirthDateException(
-                    //   context,
-                    //   state.exception,
-                    // ),
+                      AppTextFormField(
+                        labelText: context.l10n.profile_firstname_title,
+                        keyboardType: .name,
+                        autofocus: true,
+                        textInputAction: .next,
+                        errorText: _translateFirstNameException(
+                          context,
+                          state.exception,
+                        ),
+                        onChange: (value) => bloc.add(FirstNameChanged(value)),
+                      ),
+                      AppTextFormField(
+                        labelText: context.l10n.profile_lastname_title,
+                        keyboardType: .name,
+                        autofocus: true,
+                        textInputAction: .next,
+                        errorText: _translateLastNameException(
+                          context,
+                          state.exception,
+                        ),
+                        onChange: (value) => bloc.add(LastNameChanged(value)),
+                      ),
+                      GenderSelection(
+                        selected: {state.gender},
+                        onSelectionChanged: (gender) =>
+                            bloc.add(GenderChanged(gender.single)),
+                      ),
+                      AppTextFormField(
+                        labelText: context.l10n.profile_optional_email_title,
+                        keyboardType: .emailAddress,
+                        autofocus: true,
+                        textInputAction: .done,
+                        errorText: _translateEmailException(
+                          context,
+                          state.exception,
+                        ),
+                        onChange: (value) => bloc.add(EmailChanged(value)),
+                      ),
+                      AppTextFormField(
+                        controller: _birthDateController,
+                        onTap: () {
+                          AppModalBottomSheet.show(
+                            context,
+                            child: Column(
+                              mainAxisSize: .min,
+                              children: [
+                                PersianCalendarDatePicker(
+                                  initialDate: null,
+                                  firstDate: Jalali(1300),
+                                  lastDate: Jalali.now(),
+                                  initialCalendarMode: .year,
+                                  onDateChanged: (Jalali picked) {
+                                    _birthDateController.text = picked
+                                        .formatShortDate();
+                                    bloc.add(
+                                      BirthDateChanged(picked.toDateTime()),
+                                    );
+                                  },
+                                ),
+                                Padding(
+                                  padding: const .symmetric(
+                                    vertical: 8.0,
+                                    horizontal: 16.0,
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment: .end,
+                                    spacing: 8,
+                                    children: [
+                                      Expanded(
+                                        child: AppOutlinedButton.secondary(
+                                          onPressed: () => context.pop(),
+                                          title:
+                                              context.l10n.global_button_cancel,
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: AppFilledButton.secondary(
+                                          onPressed: () => context.pop(),
+                                          title:
+                                              context.l10n.global_button_save,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                        labelText: context.l10n.profile_birthdate_title,
+                        canRequestFocus: false,
+                        readOnly: true,
+                        prefixIcon: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: IconWidget(
+                            icon: AssetIcons.calendar,
+                            height: 10,
+                            width: 10,
+                          ),
+                        ),
+                        // errorText: _translateBirthDateException(
+                        //   context,
+                        //   state.exception,
+                        // ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                Spacer(),
+                Padding(
+                  padding: const .only(bottom: 16.0),
+                  child: AppFilledButton(
+                    title: context.l10n.profile_signup_button,
+                    onPressed: state.isValid
+                        ? () => bloc.add(const SubmitProfile())
+                        : null,
+                    isLoading: state.status == .loading,
+                    isWide: true,
+                  ),
+                ),
+              ],
             ),
           ),
-          floatingActionButton: AppFilledButton(
-            title: context.l10n.profile_signup_button,
-            onPressed: state.isValid
-                ? () => bloc.add(const SubmitProfile())
-                : null,
-            isLoading: state.status == .loading,
-            isWide: true,
-          ),
-          floatingActionButtonLocation: .centerFloat,
         );
       },
     );
